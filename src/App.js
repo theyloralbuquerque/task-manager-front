44e71103ea4f727cd2559001b2b2623a3bf94633
Theylor Albuquerque <theylor.oliveira@icc.org.br>
2024-12-17 16:30:25 -0300

    feat: fetch tasks from back-end; clean useless files

--- src/App.js
+++ src/App.js
@@ -1,71 +1,42 @@
-// import { useState } from 'react';
-// import TaskItem from './components/TaskItem';
-
-// const App = () => {
-//     const [tasks, setTasks] = useState([
-//         {
-//             id: '1',
-//             description: 'Estudar Programação',
-//             isCompleted: false,
-//         },
-//         {
-//             id: '2',
-//             description: 'Ler',
-//             isCompleted: true,
-//         },
-//     ]);
-
-//     return (
-//         <>
-//             {tasks.map((task) => (
-//                 <TaskItem key={task.id} task={task} />
-//             ))}
-//         </>
-//     );
-// };
-
-// export default App;
-
-import React from 'react';
+import { useEffect, useState } from 'react';
 import TaskItem from './components/TaskItem';
+import axios from 'axios';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleStateChange = this.handleStateChange.bind(this);
+const App = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: '1',
+            description: 'Estudar Programação',
+            isCompleted: false,
+        },
+        {
+            id: '2',
+            description: 'Ler',
+            isCompleted: true,
+        },
+    ]);
 
-        this.state = {
-            tasks: [
-                {
-                    id: '1',
-                    description: 'Estudar Programação',
-                    isCompleted: false,
-                },
-                {
-                    id: '2',
-                    description: 'Ler',
-                    isCompleted: true,
-                },
-            ],
-        };
-    }
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:8000/tasks');
 
-    handleStateChange() {
-        this.setState({
-            tasks: [],
-        });
-    }
+            setTasks(data);
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
-    render() {
-        return (
-            <>
-                {this.state.tasks.map((task) => (
-                    <TaskItem key={task.id} task={task} />
-                ))}
-                <button onClick={this.handleStateChange}>Limpar tarefas</button>
-            </>
-        );
-    }
-}
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    return (
+        <>
+            {tasks.map((task) => (
+                <TaskItem key={task.id} task={task} />
+            ))}
+        </>
+    );
+};
 
 export default App;
